e63b43a4848ace402c6368fec015dcbf91b5d008
jmcolella <colella.john@gmail.com>
2016-06-24 20:22:32 -0400

    complete reqest spec tests for categories

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,17 +1,18 @@
-# require 'rails_helper'
-#
-# RSpec.describe "Games", :type => :request do
-#   describe "GET /games" do
-#     it "is successful" do
-#       get games_path
-#       expect(response.status).to be(200)
-#     end
-#   end
-#
-#   describe "GET /new" do
-#     it "is successful" do
-#       get new_game_path
-#       expect(response.status).to be(200)
-#     end
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe "Categories", :type => :request do
+  let( :category ) { Category.create!( title: "drama" ) }
+  describe "GET /categories" do
+    it "is successful" do
+      get categories_path
+      expect(response.status).to be(200)
+    end
+  end
+
+  describe "GET /show" do
+    it "is successful" do
+      get category_path( category )
+      expect(response.status).to be(200)
+    end
+  end
+end
